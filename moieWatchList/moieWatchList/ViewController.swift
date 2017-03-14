30a685e42dfe3e80d3ce136cda748ba50f0bcd58
RogelioBarcenas12 <rbarcen2@asu.edu>
2017-03-14 12:33:56 -0700

    project files
    Uploaded initial project files

---
+++ moieWatchList/moieWatchList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  moieWatchList
+//
+//  Created by Rogelio Barcenas on 3/14/17.
+//  Copyright © 2017 Rogelio Barcenas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
